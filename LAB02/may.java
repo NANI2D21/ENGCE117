f2d03dc3753c88cc61c9bcf8a7263467525f18e3
MAYSA <118293596+NANI2D21@users.noreply.github.com>
2023-01-09 19:08:04 +0700

    Create may.java

---
+++ LAB02/may.java
@@ -0,0 +1,7 @@
+public class nine1 {    
+    public static void main(String[] args){
+        String fullName = "Ariya Ranya";
+        System.out.println("My name is : " +fullName );
+        System.out.println("\n Length of FullName : "+fullName.length());
+    }
+}
